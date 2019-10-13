046aacb0a6be81dfdb3363e82e7ae4d89aaa6bba
DestinyJun <429771890@qq.com>
2019-10-13 23:42:04 +0800

    操作日志： （1）模板引擎

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -9,6 +9,17 @@ class IndexController extends Controller
   public function index()
   {
 //    $this->display();
-    $this->display('./index.html');  // 指定模板文件
+    $this->display('index');  // 指定模板文件
+  }
+  public function add()
+  {
+    $img = 'h哈哈';
+    $data  = array(
+      'name'=> 'wenjun',
+      'version'=> '3.2',
+    );
+    $this->assign('img',$img); // 第一种
+    $this->data = $data; // 第二种
+    $this->display();
   }
 }
