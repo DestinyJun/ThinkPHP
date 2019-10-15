17f52d1c3c9c3c65c4851fb5e8800185f4e41669
DestinyJun <429771890@qq.com>
2019-10-15 23:29:31 +0800

    操作日志： （1）TP数据库配置

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,8 +1,15 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
-  'TMPL_PARSE_STRING'=>array(
+  //'配置项'=>'配置值'
+  'TMPL_PARSE_STRING' => array(
     '__PUBLIC_ADMIN__' => '/Public/Admin', // 后台静态资源目录
-    '__PUBLIC_HOME__' => '/Public/Home'    // 前台静态资源目录
-  )
+    '__PUBLIC_HOME__' => '/Public/Home',    // 前台静态资源目录
+  ),
+  'DB_TYPE' => '',     // 数据库类型
+  'DB_HOST' => '', // 服务器地址
+  'DB_NAME' => '',          // 数据库名
+  'DB_USER' => '',      // 用户名
+  'DB_PWD' => '',          // 密码
+  'DB_PORT' => '',        // 端口
+  'DB_PREFIX' => '',    // 数据库表前缀
 );
